1d3c3344f24c099ab255e0d97127f6e0cd3a9eff
Godsplan Elot <elotgodsplan2@gmail.com>
2025-08-29 20:28:44 +0100

    Create Grid Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Button, HStack } from "@chakra-ui/react"
+import {  Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react"
 
 function App() {
-  
+  const showAside = useBreakpointValue({ base: false, lg: true });
 
-  return (
-        <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
-  )
+  return <Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"`
+  }}>
+    <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <Show when={showAside}>
+        <GridItem area='aside' bg='gold'>Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+  </Grid>
 }
 
 export default App
